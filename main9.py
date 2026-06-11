9b311d83ac8446b4ad30195fa1001bb4e016fc47
tikitica2-ops <tikitica2@gmail.com>
2026-06-11 17:09:52 +0900

    Add files via upload

--- main9.py
+++ main9.py
@@ -107,7 +107,7 @@ if uploaded_file is not None:
                 handler = StreamHandler(      chat_box       )
 
                 llm = ChatOpenAI(
-                    model="gpt-4.1-mini",
+                    model="gpt-4o-mini",
                     temperature=0,
                     api_key=openai_key,
                     streaming=True,
